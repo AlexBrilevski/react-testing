f1f1dfb9f575d84f28bf9e1233f7a2055917994d
Alex Brilevski <brilevski.alex@gmail.com>
2026-02-27 21:28:18 +0300

    Tests to check state changes in the Greeting component were added

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 const Greeting = () => {
+  const [changeText, setChangeText] = useState(false);
+
   return (
     <div>
-      <h2>Hello world!</h2>
-      <p>It's good to see you!</p>
+      <h2>Hello World!</h2>
+      {!changeText ?
+        <p>It's good to see you!</p>
+        :
+        <p>Check out this new text!</p>
+      }
+      <button onClick={() => setChangeText(true)}>
+        Change text
+      </button>
     </div>
   );
 };
